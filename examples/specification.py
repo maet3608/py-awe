3a3156e7432923b924b87e5b5cda862136304366
Stefan Maetschke <stefanrm@au1.ibm.com>
2018-07-10 15:56:34 +1000

    missing specification file added

---
+++ examples/specification.py
@@ -0,0 +1,26 @@
+SPEC = \
+{
+  "input": [
+    {
+      "name": "first number",
+      "type": "int"
+    }, 
+    {
+      "name": "second number",
+      "type": "int"
+    }
+  ],
+  "output": [
+    {
+      "name": "number sum",
+      "type": "int"
+    }
+  ],
+  "package": {
+    "author": "Stefan Maetschke", 
+    "author_email": "stefan.maetschke@gmail.com", 
+    "description": "Adds two numbers",
+    "type": "Adder",
+    "version": "1.0.0"
+  },
+}
